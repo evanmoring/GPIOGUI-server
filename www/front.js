bf1f1ff18247064baa9b46ba9f28edeaf28aab94
evanmoring <54807675+evanmoring@users.noreply.github.com>
2020-04-08 12:48:22 -0400

    Added i2c functionality
    Also added a tarball with the modules and node for ease of use

--- www/front.js
+++ www/front.js
@@ -1,7 +1,7 @@
 var boardType = [false,0,1,3,1,3,2,3,3,2,3,3,3,3,2,3,3,0,3,3,2,3,3,3,3,2,3,4,4,3,2,3,3,3,2,3,3,3,3,2,3];
 var boardType = [false,0,1,3,1,3,2,3,3,2,3,3,3,3,2,3,3,0,3,3,2,3,3,3,3,2,3,4,4,3,2,3,3,3,2,3,3,3,3,2,3];
 var pinTypeKey = ['3V3','5v','GND','GPIO','Empty'];
-var bcmDict = {3:2,5:3,7:4,8:14,10:15,11:17,12:18,13:27,15:22,16:23,18:24,19:10,21:9,22:25,23:11,24:8,26:7,29:5,31:6,32:12,33:13,35:19,36:16,37:26,38:20,40:21};
+var bcmDict = {7:4,8:14,10:15,11:17,12:18,13:27,15:22,16:23,18:24,19:10,21:9,22:25,23:11,24:8,26:7,29:5,31:6,32:12,33:13,35:19,36:16,37:26,38:20,40:21};
 var voltageDict = {0:"Low", 1:"High",2:"Pin not set"};
 var $ = function( id ) { return document.getElementById( id ); };
 
@@ -17,14 +17,12 @@ function loadData() {
 
 function readForm(){
     let formDict ={};
-	console.log('emitted form');
     formDict.bcmPin=pinSelect.value;
     formDict.voltage=voltageSelect.value;
     socket.emit('form',formDict);
 }
 
 function showI2C(){
-    console.log('function')
     if ($("i2cControls").style.display=="block"){
         $("i2cControls").style.display="none";
         $("showI2CButton").value = "Show I2C"
@@ -57,7 +55,6 @@ function submitI2C(){
         return;
     }
         
-    console.log(d);
 $("i2cReadout").innerHTML="Data sent";
     socket.emit('i2cForm',d);
     
@@ -78,7 +75,6 @@ $("i2cReadout").innerHTML="Data sent";
     
     function checkSub (input,max){
         if (isNaN(input) || input > max){
-            console.log('check was bad');
             return true;  
         }
     }
@@ -111,10 +107,12 @@ function createPins(pinList) {
     }
 	$("direction3").innerHTML="i2c"
 	$("direction3").style.color="brown"
+	$("bcm3").innerHTML = "2"
 	$("voltage3").innerHTML="SDA"
 	$("voltage3").style.color="brown"
 	$("direction5").innerHTML="i2c"
 	$("direction5").style.color="brown"
+	$("bcm5").innerHTML = "3"
 	$("voltage5").innerHTML="SCL"
 	$("voltage5").style.color="brown"
 }
@@ -187,7 +185,6 @@ function createPinGridHeader () {
 var socket = io.connect();
 
 socket.on('i2cReturn',function(data){
-	console.log(data);
 	let newString = 'Data received: ';
 	for(let i = 0; i < data.length; i++){
 		if(i % 2 != 0){newString += (data[i]+' ').toUpperCase();}
@@ -213,7 +210,6 @@ socket.on('i2cScanReturn',function(scanList){
 });
 
 socket.on('pinData',function(data){
-	console.log('r data');
     $('voltage'+data.boardNumber).innerHTML=data.voltage;
     if(data.voltage==1){
         $('voltage'+data.boardNumber).style.color='red';
